c16fee16c425ede11a145790579080db676e7c3e
saikinkatenai95 <saikinkatenai95@outlook.jp>
2021-11-13 17:59:55 +0900

    indexにyoutube動画を表示

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,7 +1,31 @@
 class VideosController < ApplicationController
 
+require 'google/apis/youtube_v3'
+require 'active_support/all'
+
+GOOGLE_API_KEY=ENV["YOUTUBE_SECRET_KEY"]
+
+def find_video(keyword, after: 1.months.ago, before: Time.now)
+  service = Google::Apis::YoutubeV3::YouTubeService.new
+  service.key = GOOGLE_API_KEY
+
+  next_page_token = nil
+  begin
+  opt = {
+    q: keyword,
+    type: 'video',
+    max_results: 3,
+    order: :date,
+    page_token: next_page_token,
+    published_after: after.iso8601,
+    published_before: before.iso8601
+  }
+    service.list_searches(:snippet, opt)
+  end
+end
+
   def index
-    @videos = Video.all
+    @youtube_data = find_video('APEX')
   end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root to: "videos#index"
   get 'videos/index'
+
+  resources :videos, only: :index
 end
