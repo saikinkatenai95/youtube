d0522b0218c52e349da645f3cf1db303df074478
saikinkatenai95 <saikinkatenai95@outlook.jp>
2021-12-09 21:41:34 +0900

    検索バー改変

--- app/models/choice.rb
+++ app/models/choice.rb
@@ -1,7 +1,7 @@
 class Choice < ActiveHash::Base
   self.data = [
-    { id: 1, name: '--'},
-    { id: 2, name: '〜を含めない'},
+    { id: 1, name: '両方含めて表示'},
+    { id: 2, name: 'を含めない'},
     { id: 3, name: 'いずれかを表示'},
     { id: 4, name: '完全一致'}
   ]
